f307b229bdeae64e6ddaaba920e1784ed04d9b09
zhenpeng_liu <zhengpeng_liu@kingdee.com>
2025-10-22 18:15:08 +0800

    测试用例支持释放bs.logout

--- tests/test_initapp.py
+++ tests/test_initapp.py
@@ -1,7 +1,9 @@
 import pytest
 from app.core.register import register_app
+from app.api import stock
 
 @pytest.fixture(scope="module")
 def registerapp():
     application = register_app()
-    return application
+    yield  application
+    stock.bsLogout()
